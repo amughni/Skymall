0b688603923885508d235ea3ad08f50d661619f6
amughni <amughni@gmail.com>
2014-05-07 23:45:58 -0700

    config changes for MySQL

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -5,6 +5,17 @@ dataSource {
     username = "sa"
     password = ""
 }
+
+//dataSource {
+//    pooled = true
+//    dbCreate = "update"
+//    url = "jdbc:mysql://localhost:3306/skymall"
+//    driverClassName = "com.mysql.jdbc.Driver"
+//    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+//    username = "mysql"
+//    password = "mysql"
+//}
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -18,13 +29,14 @@ environments {
     development {
         dataSource {
             dbCreate = "create" // one of 'create', 'create-drop', 'update', 'validate', ''
+//            url = "jdbc:mysql://localhost:3306/skymall"
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://localhost:3306/skymall;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     production {
